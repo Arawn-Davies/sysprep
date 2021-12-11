26db2afb65841f0debcc7c7cb27834f5d5fb475f
Arawn-Davies <arawn.davies@gmail.com>
2021-12-11 21:02:47 +0000

    changed relative path to .bashrc to absolute path

--- runme.sh
+++ runme.sh
@@ -6,8 +6,8 @@ php7.4-phpdbg php7.4-xdebug docker
 
 git clone https://github.com/Arawn-Davies/sysprep
 cd sysprep
-cat .bashrc >> ../.bashrc
-cat .bash_aliases >> ../.bash_aliases
+cat .bashrc >> $HOME/bashrc
+cat .bash_aliases >> $HOME/.bash_aliases
 
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
