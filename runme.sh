b3c94752808fd79028d288920de93564c21efb43
Arawn-Davies <arawn.davies@gmail.com>
2021-12-13 01:33:27 +0000

    cd back up to parent

--- runme.sh
+++ runme.sh
@@ -8,7 +8,7 @@ git clone https://github.com/Arawn-Davies/sysprep
 cd sysprep
 cat .bashrc >> $HOME/bashrc
 cat .bash_aliases >> $HOME/.bash_aliases
-
+cd..
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
