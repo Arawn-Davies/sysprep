82548be76ddda21e73d48579bbcfead2ad77a586
Arawn (Ari) Davies <19764552+Arawn-Davies@users.noreply.github.com>
2025-01-21 01:33:54 +0000

    Update runme.sh
    Comment out PHP7.4 stuff - deprecated & out of support
    Fix .bashrc pipe

--- runme.sh
+++ runme.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 sudo apt install git gcc g++ gdb binutils build-essential qemu-system-x86 xorriso grub-pc grub-common mtools nasm texinfo flex bison wget curl apache2 libapache2-mod-php \
-mariadb-server mariadb-client php7.4-bz2 php7.4-cli php7.4-common php7.4-curl php7.4-mysql php7.4-readline php7.4-xml php7.4-zip php7.4-mbstring php7.4-opcache \
-php7.4-phpdbg php7.4-xdebug docker
+mariadb-server mariadb-client docker
 
-git clone https://github.com/Arawn-Davies/sysprep
-cd sysprep
-cat .bashrc >> $HOME/bashrc
+#git clone https://github.com/Arawn-Davies/sysprep
+#cd sysprep
+cat .bashrc >> $HOME/.bashrc
 cat .bash_aliases >> $HOME/.bash_aliases
 cd..
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
