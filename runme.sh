00224d5442ac3eff7e1bd1ad834dd4cad73f7d26
Arawn (Ari) Davies <19764552+Arawn-Davies@users.noreply.github.com>
2025-01-23 23:35:40 +0000

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-sudo apt install git gcc g++ gdb binutils build-essential qemu-system-x86 xorriso grub-pc grub-common mtools nasm texinfo flex bison wget curl apache2 libapache2-mod-php \
-mariadb-server mariadb-client docker
+sudo apt install git gcc g++ gdb binutils build-essential qemu-system-x86 xorriso grub-pc grub-common mtools nasm texinfo flex bison wget curl
 
 #git clone https://github.com/Arawn-Davies/sysprep
 #cd sysprep
 cat .bashrc >> $HOME/.bashrc
 cat .bash_aliases >> $HOME/.bash_aliases
-cd..
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
