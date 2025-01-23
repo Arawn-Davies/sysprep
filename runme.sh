f2bdebddab19e2eac1f7b549fb4e0062068eb11b
Arawn (Ari) Davies <19764552+Arawn-Davies@users.noreply.github.com>
2025-01-23 23:40:32 +0000

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -4,8 +4,10 @@ sudo apt install git gcc g++ gdb binutils build-essential qemu-system-x86 xorris
 
 #git clone https://github.com/Arawn-Davies/sysprep
 #cd sysprep
-cat .bashrc >> $HOME/.bashrc
-cat .bash_aliases >> $HOME/.bash_aliases
+wget https://raw.githubusercontent.com/Arawn-Davies/sysprep/refs/heads/master/.bash_aliases -O bash_aliases
+wget https://raw.githubusercontent.com/Arawn-Davies/sysprep/refs/heads/master/.bashrc -O bashrc
+cat bashrc >> $HOME/.bashrc && rm bashrc
+cat bash_aliases >> $HOME/.bash_aliases && rm bash_aliases
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
